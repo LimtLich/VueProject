2154d194a591acb3d42fdd8c3d641e2c14d6a62b
limt <921136839@qq.com>
2017-05-10 11:37:19 +0800

    page css fix

--- src/components/Album.vue
+++ src/components/Album.vue
@@ -1,12 +1,21 @@
 <template>
 <div>
-  <el-upload class="upload-demo" drag action="https://jsonplaceholder.typicode.com/posts/" multiple>
-    <i class="el-icon-upload"></i>
-    <div class="el-upload__text">将文件拖到此处，或
-      <em>点击上传</em>
+  <el-button class="showUpload" @click="showupload = !showupload" type="info">{{head_title}}
+    <i :class="head_icon"></i>
+  </el-button>
+  <transition name="el-zoom-in-top">
+    <div v-show="showupload" class="imgupload">
+      <el-upload class="uploadDiv" action="https://jsonplaceholder.typicode.com/posts/" list-type="picture-card" :on-preview="handlePictureCardPreview" :on-remove="handleRemove">
+        <i class="el-icon-plus"></i>
+      </el-upload>
+      <el-dialog v-model="dialogVisible" size="tiny">
+        <img width="100%" :src="dialogImageUrl" alt="">
+      </el-dialog>
+      <el-button @click="showupload = !showupload" :plain="true" type="info">Commit upload
+        <i class="el-icon-upload el-icon--right"></i>
+      </el-button>
     </div>
-    <div class="el-upload__tip" slot="tip">只能上传jpg/png文件，且不超过500kb</div>
-  </el-upload>
+  </transition>
 </div>
 </template>
 
@@ -14,11 +23,33 @@
 export default {
   data() {
     return {
-
+      fileList2: [],
+      showupload: false,
+      dialogImageUrl: '',
+      dialogVisible: false,
+      head_title: 'Picture upload',
+      head_icon:'el-icon-upload2 el-icon--right',
     }
   },
   methods: {
-
+    handleRemove(file, fileList) {
+      console.log(file, fileList);
+    },
+    handlePictureCardPreview(file) {
+      this.dialogImageUrl = file.url;
+      this.dialogVisible = true;
+    },
+  },
+  watch: {
+    showupload(val) {
+      if(val){
+        this.head_title = 'Close'
+        this.head_icon = 'el-icon-circle-close el-icon--right'
+      }else{
+        this.head_title = 'Picture upload'
+        this.head_icon = 'el-icon-upload2 el-icon--right'
+      }
+    }
   },
   created() {
 
@@ -31,5 +62,21 @@ export default {
 * {
   margin: 0;
   padding: 0;
+}
+
+.showUpload {
+  width: 100%;
+  border-radius: inherit!important;
+}
+
+.imgupload {
+  border-left: 1px solid #dfdfdf;
+  height: 100%;
+  border: 1px solid #dfdfdf;
+  padding-bottom: 1%;
+}
+
+.uploadDiv {
+  padding: 1%;
 }
 </style>
